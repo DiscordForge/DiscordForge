820f2ed3730080f62322a05f5318978bd262be9d
LewisTehMinerz <lewisminecraft@hotmail.co.uk>
2017-07-19 18:38:57 +0100

    install asar and some others

--- index.js
+++ index.js
@@ -18,10 +18,106 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-var args = require('minimist')(process.argv.slice(2));
-var _ = args._;
+// parse commands
+var command, argv = null;
+try {
+    var {command, argv} = require('command-line-commands')([null, 'help', 'inject', 'uninject', 'plugin', 'repo', 'update']);
+} catch (err) {
+    console.log('error: unknown command - try \'discordforge help\'');
+    return;
+}
+// require deps
+const options = require('minimist')(argv);
+const usage = require('command-line-usage');
+const ps = require('ps-node');
+
+// help page
+const help = [
+    {
+        header: 'DiscordForge',
+        content: 'A toolkit that allows you to create and install your own plugins into the Discord client.'
+    },
+    {
+        header: 'Synopsis',
+        content: [
+            '$ discordforge [[bold]{command}] [...]'
+        ]
+    },
+    {
+        header: 'Commands',
+        content: [
+            {
+                name: 'help',
+                summary: 'Display help information about DiscordForge.'
+            },
+            {
+                name: 'inject',
+                summary: 'Injects DiscordForge into the running Discord process\'s app.asar.'
+            },
+            {
+                name: 'uninject',
+                summary: 'Reverses the operation of inject.'
+            },
+            {
+                name: 'plugin [[bold]{command}] ...',
+                summary: 'Plugin management.'
+            },
+            {
+                name: 'repo [[bold]{command}] ...',
+                summary: 'Repository management.'
+            },
+            {
+                name: 'update',
+                summary: 'Updates DiscordForge.'
+            },
+        ]
+    },
+    {
+        header: 'Plugin Management',
+        content: [
+            {
+                name: 'These commands allow you to manage plugins.\n'
+            },
+            {
+                name: 'plugin install [[bold]{name}]',
+                summary: 'Installs a plugin.'
+            },
+            {
+                name: 'plugin uninstall [[bold]{name}]',
+                summary: 'Uninstalls a plugin.'
+            },
+            {
+                name: 'plugin list',
+                summary: 'Lists all installed plugins.'
+            }
+        ]
+    },
+    {
+        header: 'Repository Management',
+        content: [
+            {
+                name: 'These commands allow you to manage repositories.\n'
+            },
+            {
+                name: 'repo add [[bold]{name}]',
+                summary: 'Adds a repository to search plugins for.'
+            },
+            {
+                name: 'repo remove [[bold]{name}]',
+                summary: 'Removes a repository.'
+            },
+            {
+                name: 'repo list',
+                summary: 'Lists all repositories.'
+            }
+        ]
+    }
+]
+var _ = options._;
 
 // no args specified or command "help"
-if (_[0] == undefined || _[0] == "help") {
-    console.log("test");
+if (command == null || command == 'help') {
+    console.log(usage(help));
+} else if (command == 'inject') {
+    ps.lookup({command})
 }
